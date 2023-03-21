b1720c43df492e8aab1309a25e65159294002d1a
Jan Grosicki <grosicki.jan@gmail.com>
2023-03-21 22:07:12 +0100

    TicTacToeV1.2, big changes to game's logic.

--- 1.ticTacToe/script.js
+++ 1.ticTacToe/script.js
@@ -1,70 +1,58 @@
+"use scrict";
+
 //When player wins, this boolean is changed to false, disabling player interaction
 let gameRunning = true;
 
+//game always starts with the circle.
+let turn = "O";
+
+//initializing array needed for game logic with empty strings
+const cells = new Array(9);
+for (let i = 0; i < cells.length; i++) cells[i] = "";
+
+const cellNames = [
+    "#cellOne",
+    "#cellTwo",
+    "#cellThree",
+    "#cellFour",
+    "#cellFive",
+    "#cellSix",
+    "#cellSeven",
+    "#cellEight",
+    "#cellNine"
+];
+
 //Listen, if TicTacToe cells are being clicked
-document.querySelector("#cellOne").addEventListener("click", () => {
-    handleClick("#cellOne");
-});
-document.querySelector("#cellTwo").addEventListener("click", () => {
-    handleClick("#cellTwo");
-});
-document.querySelector("#cellThree").addEventListener("click", () => {
-    handleClick("#cellThree");
-});
-document.querySelector("#cellFour").addEventListener("click", () => {
-    handleClick("#cellFour");
-});
-document.querySelector("#cellFive").addEventListener("click", () => {
-    handleClick("#cellFive");
-});
-document.querySelector("#cellSix").addEventListener("click", () => {
-    handleClick("#cellSix");
-});
-document.querySelector("#cellSeven").addEventListener("click", () => {
-    handleClick("#cellSeven");
-});
-document.querySelector("#cellEight").addEventListener("click", () => {
-    handleClick("#cellEight");
-});
-document.querySelector("#cellNine").addEventListener("click", () => {
-    handleClick("#cellNine");
-});
+for (let cellName of cellNames) {
+    document.querySelector(cellName).addEventListener("click", () => {
+        handleClick(cellName);
+    });
+}
 
-const resetGame = function () {
-    //resetting the message below the game
-    document.querySelector(".winMessage").textContent = "";
-    //resetting the turn to default value
-    document.querySelector(".turn").textContent = "O";
-    document.querySelector(".turn").style.color = "aqua";
-
-    //resetting tiles
-    document.querySelector("#cellOne").textContent = ".";
-    document.querySelector("#cellOne").style.color = "#222";
-    document.querySelector("#cellTwo").textContent = ".";
-    document.querySelector("#cellTwo").style.color = "#222";
-    document.querySelector("#cellThree").textContent = ".";
-    document.querySelector("#cellThree").style.color = "#222";
-    document.querySelector("#cellFour").textContent = ".";
-    document.querySelector("#cellFour").style.color = "#222";
-    document.querySelector("#cellFive").textContent = ".";
-    document.querySelector("#cellFive").style.color = "#222";
-    document.querySelector("#cellSix").textContent = ".";
-    document.querySelector("#cellSix").style.color = "#222";
-    document.querySelector("#cellSeven").textContent = ".";
-    document.querySelector("#cellSeven").style.color = "#222";
-    document.querySelector("#cellEight").textContent = ".";
-    document.querySelector("#cellEight").style.color = "#222";
-    document.querySelector("#cellNine").textContent = ".";
-    document.querySelector("#cellNine").style.color = "#222";
-
-    gameRunning = true;
-};
-
-//Listen, if reset button is clicked
-
+//Listen, if reset button is being clicked
 document.querySelector(".btn").addEventListener("click", () => {
     resetGame();
 });
+
+//  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+
+const resetGame = function () {
+    gameRunning = false;
+    //resetting the message below the game
+    document.querySelector(".winMessage").textContent = "";
+    //resetting the turn to default value
+    turn = "O";
+    document.querySelector(".turn").textContent = "O";
+    document.querySelector(".turn").style.color = "aqua";
+
+    //resetting tiles and emptying the cells
+    for (let i = 0; i < cells.length; i++) {
+        cells[i] = "";
+        document.querySelector(cellNames[i]).textContent = ".";
+        document.querySelector(cellNames[i]).style.color = "#222";
+    }
+    gameRunning = true;
+};
 
 const victory = (str) => {
     gameRunning = false;
@@ -87,80 +75,57 @@ const checkForWin = function (player) {
     if (
         //are there any Vertical wins?
         // 1,4,7
-        (document.querySelector("#cellOne").textContent === player &&
-            document.querySelector("#cellFour").textContent === player &&
-            document.querySelector("#cellSeven").textContent === player) ||
+        (cells[0] === player && cells[3] === player && cells[6] === player) ||
         // 2,5,8
-        (document.querySelector("#cellTwo").textContent === player &&
-            document.querySelector("#cellFive").textContent === player &&
-            document.querySelector("#cellEight").textContent === player) ||
+        (cells[1] === player && cells[4] === player && cells[7] === player) ||
         // 3,6,9
-        (document.querySelector("#cellThree").textContent === player &&
-            document.querySelector("#cellSix").textContent === player &&
-            document.querySelector("#cellNine").textContent === player) ||
+        (cells[2] === player && cells[5] === player && cells[8] === player) ||
         //are there any Horizontal wins?
         // 1,2,3
-        (document.querySelector("#cellOne").textContent === player &&
-            document.querySelector("#cellTwo").textContent === player &&
-            document.querySelector("#cellThree").textContent === player) ||
+        (cells[0] === player && cells[1] === player && cells[2] === player) ||
         // 4,5,6
-        (document.querySelector("#cellFour").textContent === player &&
-            document.querySelector("#cellFive").textContent === player &&
-            document.querySelector("#cellSix").textContent === player) ||
+        (cells[3] === player && cells[4] === player && cells[5] === player) ||
         // 7,8,9
-        (document.querySelector("#cellSeven").textContent === player &&
-            document.querySelector("#cellEight").textContent === player &&
-            document.querySelector("#cellNine").textContent === player) ||
+        (cells[6] === player && cells[7] === player && cells[8] === player) ||
         //are there any Diagonal wins?
         // 1,5,9
-        (document.querySelector("#cellOne").textContent === player &&
-            document.querySelector("#cellFive").textContent === player &&
-            document.querySelector("#cellNine").textContent === player) ||
+        (cells[0] === player && cells[4] === player && cells[8] === player) ||
         // 3,5,7
-        (document.querySelector("#cellThree").textContent === player &&
-            document.querySelector("#cellFive").textContent === player &&
-            document.querySelector("#cellSeven").textContent === player)
+        (cells[2] === player && cells[4] === player && cells[6] === player)
     ) {
         victory(player);
         return;
     }
 
-    /*Check if the game ended in a draw
-        -are all the cells in the table claimed?
-    */
-    if (
-        document.querySelector("#cellOne").textContent !== "." &&
-        document.querySelector("#cellTwo").textContent !== "." &&
-        document.querySelector("#cellThree").textContent !== "." &&
-        document.querySelector("#cellFour").textContent !== "." &&
-        document.querySelector("#cellFive").textContent !== "." &&
-        document.querySelector("#cellSix").textContent !== "." &&
-        document.querySelector("#cellSeven").textContent !== "." &&
-        document.querySelector("#cellEight").textContent !== "." &&
-        document.querySelector("#cellNine").textContent !== "."
-    ) {
-        victory("draw");
-        return;
-    }
+    /*Check if the game ended in a draw -is there still a possible move? */
+    for (let cell of cells) if (cell === "") return;
+
+    victory("draw");
 };
 
 const handleClick = function (str) {
-    //check if there is a figure in the cell
-    if (!gameRunning || document.querySelector(str).textContent !== ".") return;
+    //get the index of the clicked cell
+    let pos = cellNames.indexOf(str);
+    //check if there is 'O  or 'X' in the cell and if the game is running
+    if (!gameRunning || cells[pos] !== "") return;
 
     //If it is the Circle's turn
-    if (document.querySelector(".turn").textContent === "O") {
+    if (turn === "O") {
+        cells[pos] = "O";
         document.querySelector(str).textContent = "O";
         document.querySelector(str).style.color = "aqua";
+        turn = "X";
         document.querySelector(".turn").textContent = "X";
         document.querySelector(".turn").style.color = "red";
         checkForWin("O");
         return;
     }
     //If it is the Cross's turn
-    if (document.querySelector(".turn").textContent === "X") {
+    if (turn === "X") {
+        cells[pos] = "X";
         document.querySelector(str).textContent = "X";
         document.querySelector(str).style.color = "red";
+        turn = "O";
         document.querySelector(".turn").textContent = "O";
         document.querySelector(".turn").style.color = "aqua";
         checkForWin("X");
