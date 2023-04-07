a3893c77fb225a9898a11a9276cd71928e61df33
Jan Grosicki <grosicki.jan@gmail.com>
2023-04-07 23:06:53 +0200

    economy and betting

--- 2.blackJackTable/script.js
+++ 2.blackJackTable/script.js
@@ -1,59 +1,101 @@
 "use strict";
 
 /*
-Version 0.1 of Black jack game.
-02.04.2023 | Jan Grosicki | https://github.com/kenayv
+Version 0.2 of Black jack game.
+07.04.2023 | Jan Grosicki | https://github.com/kenayv
 
+Changelist:
+    * Added little economy and betting
+    * Improved readability and logic.
+    * Cards are now stored in arrays (dealerCards,playerCards), and not in variables such as plCard1, plCard2 (...)
+    *Colors hex names are stored in variables
+    * Background does not change anymore size on viewport size change.
+    * Added very little responsivity
+    * Dealer waits 1000 ms between drawing each card to make the game more interactive
+    
 Notes:
     * Probably contains a lot of redundant code
     * Might contain bugs
     * Playing more than one game requires reloading the page
     * Not responsive! (Best suited for 360x720 monitors)
     * It is possible that 2 identical cards are drawn.
-        - Theoretically, the game could be played with many decks. That shouldn't be a problem.
+     - Theoretically, the game could be played with many decks. That shouldn't be a problem.
         - I could add some logic that actually stores an array of cards and checks if it has been drawn. This would make card-counting possible.
-    
+
 To be added:
-    * Money and betting
+    * Game should automatically restart after playing a round.
+    * Money changes:
         - Should it be stored elsewhere?
         - What technology could I use to store it in external files?
         - <button pass </button>
         - <button Double-down </button>
     * RWD
-    * Dealer should take ~300-500 ms between drawing each card 
-        - For visual effects, to make the game look better
-    * Background shouldn't change size on viewport size change.
-    * Comments, comments, comments!!!
+    * getRandomCardUrl LOGIC SHOULD BE CHANGED - more info at the function
 */
 
+//variable names are easier to read than hex
+const colorGreen = "#25d53d";
+const colorRed = "#d53d25";
+
 const dealerDeck = document.getElementById("deck");
-const dealerCard1 = document.getElementById("dealer-card1");
-const dealerCard2 = document.getElementById("dealer-card2");
-const dealerCard3 = document.getElementById("dealer-card3");
-const dealerCard4 = document.getElementById("dealer-card4");
-const dealerCard5 = document.getElementById("dealer-card5");
-const dealerCard6 = document.getElementById("dealer-card6");
+const dealerCards = [
+    document.getElementById("dealer-card1"),
+    document.getElementById("dealer-card2"),
+    document.getElementById("dealer-card3"),
+    document.getElementById("dealer-card4"),
+    document.getElementById("dealer-card5"),
+    document.getElementById("dealer-card6")
+];
+const playerCards = [
+    document.getElementById("player-card1"),
+    document.getElementById("player-card2"),
+    document.getElementById("player-card3"),
+    document.getElementById("player-card4"),
+    document.getElementById("player-card5"),
+    document.getElementById("player-card6")
+];
 
-const playerCard1 = document.getElementById("player-card1");
-const playerCard2 = document.getElementById("player-card2");
-const playerCard3 = document.getElementById("player-card3");
-const playerCard4 = document.getElementById("player-card4");
-const playerCard5 = document.getElementById("player-card5");
-const playerCard6 = document.getElementById("player-card6");
-
-const btnStart = document.getElementById("btn-start");
-const btnHold = document.getElementById("btn-hold");
-const btnHit = document.getElementById("btn-hit");
-
-const message = document.querySelector(".pop-up-message");
-
-let playerCardCount = 0;
 let dealerCardCount = 0;
-let playerScore = 0;
+let playerCardCount = 0;
 let dealerScore = 0;
-let gameRunning = true;
-displayMessage("Press start to play BlackJack", "#25d53d");
+let playerScore = 0;
+let betAmount = 0;
+let playerMoney = 400;
 
+const btnHit = document.getElementById("btn-hit");
+const btnHold = document.getElementById("btn-hold");
+const btnStart = document.getElementById("btn-start");
+const btnPlus50 = document.getElementById("btn-p50");
+const btnMinus50 = document.getElementById("btn-m50");
+
+const message = document.getElementById("pop-up");
+const moneyMessage = document.getElementById("player-money");
+const betMessage = document.getElementById("bet");
+moneyMessage.textContent = `$ ${playerMoney}`;
+
+let gameRunning = true;
+
+function bet() {
+    if (playerMoney < 50) return;
+    betAmount += 50;
+    playerMoney -= 50;
+    betMessage.textContent = `$ ${betAmount}`;
+    moneyMessage.textContent = `$ ${playerMoney}`;
+}
+
+function addMoney(amount) {
+    playerMoney += amount;
+    moneyMessage.textContent = `$ ${playerMoney}`;
+}
+
+function clearBet() {
+    playerMoney += betAmount;
+    betAmount = 0;
+    betMessage.textContent = `$ ${betAmount}`;
+    moneyMessage.textContent = `$ ${playerMoney}`;
+}
+
+//message displayed on the middle of the screen
 function displayMessage(content, color) {
     message.textContent = content;
     message.style.color = color;
@@ -62,15 +104,21 @@ function displayMessage(content, color) {
 
 function lossScreen() {
     gameRunning = false;
-    displayMessage("Loss!", "#d53d25");
+    betAmount = 0;
+    betMessage.textContent = "$ 0";
+    displayMessage("Loss!", colorRed);
 }
 
 function victoryScreen() {
     gameRunning = false;
-    displayMessage("Victory!", "#25d53d");
+    addMoney(betAmount * 2);
+    betAmount = 0;
+    betMessage.textContent = "$ 0";
+    displayMessage("Victory!", colorGreen);
 }
 
-function getRandomColor() {
+//Should be used ONLY in the getRandomCardUrl function
+function _getRandomColor() {
     switch (Math.trunc(Math.random() * 4)) {
         case 0:
             return "spades";
@@ -83,8 +131,9 @@ function getRandomColor() {
     }
 }
 
+// THIS LOGIC SHOULD BE CHANGED - SCORE LOGIC SHOULD BE SEPARATED FROM THE FUNCTION
 function getRandomCardUrl(who) {
-    let color = getRandomColor();
+    let color = _getRandomColor();
     let card = Math.trunc(Math.random() * 12) + 2;
 
     if (who === "player") {
@@ -114,76 +163,71 @@ function getRandomCardUrl(who) {
     return `assets/cards/${card}_of_${color}.png`;
 }
 
-function resetGame() {
-    gameRunning = true;
-    playerScore = 0;
-    dealerScore = 0;
-    playerCardCount = 2;
-    dealerCardCount = 1;
+function initCards() {
+    // card[0] = random; card[1] = card-back; hide other cards
+    dealerCards[0].src = getRandomCardUrl("dealer");
+    dealerCards[0].classList.remove("hidden");
+    dealerCards[1].src = "assets/cards/card-back.png";
+    dealerCards[1].classList.remove("hidden");
+    dealerCards[2].classList.add("hidden");
+    dealerCards[3].classList.add("hidden");
+    dealerCards[4].classList.add("hidden");
+    dealerCards[5].classList.add("hidden");
+
+    // cards [0] and [1] = random; hide other cards
+    playerCards[0].src = getRandomCardUrl("player");
+    playerCards[0].classList.remove("hidden");
+    playerCards[1].src = getRandomCardUrl("player");
+    playerCards[1].classList.remove("hidden");
+    playerCards[2].classList.add("hidden");
+    playerCards[3].classList.add("hidden");
+    playerCards[4].classList.add("hidden");
+    playerCards[5].classList.add("hidden");
+}
+
+function initgame() {
     btnStart.classList.add("hidden");
     message.classList.add("hidden");
     btnHit.classList.remove("hidden");
     btnHold.classList.remove("hidden");
-    playerCard1.src = getRandomCardUrl("player");
-    playerCard2.src = getRandomCardUrl("player");
-    dealerCard1.src = getRandomCardUrl("dealer");
-    playerCard1.classList.remove("hidden");
-    playerCard2.classList.remove("hidden");
-    dealerCard1.classList.remove("hidden");
-    dealerCard2.src = "assets/cards/card-back.png";
-    dealerCard2.classList.remove("hidden");
-    dealerCard3.classList.add("hidden");
-    dealerCard4.classList.add("hidden");
-    dealerCard5.classList.add("hidden");
-    dealerCard6.classList.add("hidden");
-    playerCard3.classList.add("hidden");
-    playerCard4.classList.add("hidden");
-    playerCard5.classList.add("hidden");
-    playerCard6.classList.add("hidden");
+    btnPlus50.classList.add("hidden");
+    btnMinus50.classList.add("hidden");
+    playerScore = 0;
+    dealerScore = 0;
+    playerCardCount = 2;
+    dealerCardCount = 1;
+    initCards();
+    gameRunning = true;
 }
 
 function dealerDraw(cardNo) {
+    if (dealerCardCount >= 6) victoryScreen();
+    dealerCardCount++;
+    if (dealerCardCount > 3) dealerDeck.classList.add("hidden");
     cardNo.src = getRandomCardUrl("dealer");
     cardNo.classList.remove("hidden");
-    dealerCardCount++;
-    if (dealerScore > 21) {
-        victoryScreen();
-        return;
-    }
-    if (dealerScore > playerScore) {
-        lossScreen();
-        return;
-    }
-    hold();
+
+    // waits 1s between drawing each card or showing a win/loss screen
+    setTimeout(() => {
+        if (dealerScore > 21) {
+            victoryScreen();
+            return;
+        }
+        if (dealerScore > playerScore) {
+            lossScreen();
+            return;
+        }
+        dealerDraw(dealerCards[dealerCardCount]);
+    }, 1000);
 }
 
 // playerCard2,3,4,5,6 variables could be stored in an array!
 function hold() {
     if (!gameRunning) return;
-    switch (dealerCardCount) {
-        case 1:
-            dealerDraw(dealerCard2);
-            break;
-        case 2:
-            dealerDraw(dealerCard3);
-            break;
-        case 3:
-            dealerDeck.classList.add("hidden");
-            dealerDraw(dealerCard4);
-            break;
-        case 4:
-            dealerDraw(dealerCard5);
-            break;
-        case 5:
-            dealerDraw(dealerCard6);
-            break;
-        default:
-            victoryScreen();
-            break;
-    }
+    dealerDraw(dealerCards[dealerCardCount]);
 }
 
-function hitCard(cardNo) {
+function drawCard(cardNo) {
     cardNo.src = getRandomCardUrl("player");
     cardNo.classList.remove("hidden");
     playerCardCount++;
@@ -192,29 +236,16 @@ function hitCard(cardNo) {
 // playerCard2,3,4,5,6 variables could be stored in an array!
 function hit() {
     if (!gameRunning) return;
-    switch (playerCardCount) {
-        case 2:
-            hitCard(playerCard3);
-            break;
-        case 3:
-            hitCard(playerCard4);
-            break;
-        case 4:
-            hitCard(playerCard5);
-            break;
-        case 5:
-            hitCard(playerCard6);
-            hold();
-            break;
-        default:
-            break;
-    }
+    drawCard(playerCards[playerCardCount]);
+
     if (playerScore > 21) {
-        dealerCard2.src = getRandomCardUrl("dealer");
+        dealerCards[1].src = getRandomCardUrl("dealer");
         lossScreen();
     }
 }
 
-btnStart.addEventListener("click", resetGame);
+btnStart.addEventListener("click", initgame);
 btnHit.addEventListener("click", hit);
 btnHold.addEventListener("click", hold);
+btnPlus50.addEventListener("click", bet);
+btnMinus50.addEventListener("click", clearBet);
