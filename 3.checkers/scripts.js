33d75e57aff282b85cfa13fbbe17f0263a7c9e9d
Jan Grosicki <grosicki.jan@gmail.com>
2023-04-09 22:07:22 +0200

    moving working, capturing not working

--- 3.checkers/scripts.js
+++ 3.checkers/scripts.js
@@ -1,15 +1,24 @@
 "use strict";
 
 /*
-Version 0.0 of checkers game.
+Version 0.02 of checkers game.
 09.04.2023 | Jan Grosicki | https://github.com/kenayv
 
+Changelist:
+    * refactoring of some code
+    * Pawn class - pawns exist in code now.
+    * working(almost) moving logic
 Notes:
-    * NOT FINISHED
-
+    * NOT FINISHED!
+    * NOT FINISHED!
+    * NOT FINISHED!
+    
 To be added:
     * RWD
-    * Moving and capturing
+    * Capturing
+    * Turns
+    * calculateMove() function bug (described in comment near function definition)
+    * Pawns bug when they reach row 8 (row 1 for red, 8 for white)
     
 */
 
@@ -17,7 +26,30 @@ const startMsg = document.querySelector(".msg");
 const btnStart = document.getElementById("btn-start");
 const checkersBoard = document.querySelector(".checkers-wrapper");
 const cells = [];
+const activeCells = [];
 let gameRunning = false;
+let selectedPawn = undefined;
+
+class Pawn {
+    constructor(id, color, currentCell) {
+        if (!id || !color || !currentCell) throw Error("bad Pawn Constructor!");
+        this.id = id;
+        this.color = color;
+        this.selected = false;
+        this.currentCell = currentCell;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+    moveFromTo(lastCell, newCell) {
+        lastCell.pawn = undefined;
+        newCell.pawn = this;
+        this.currentCell = newCell;
+        newCell.appendChild(this.id);
+    }
+}
 
 //create 64 elements, inject them into checkers-wrapper HTML element
 function initBoard() {
@@ -31,9 +63,8 @@ function initBoard() {
             if ((i + j) % 2 !== 0) {
                 newCell.classList.add("black");
                 //players can move only on black cells
-                newCell.addEventListener("click", scanForMoves);
-                newCell.hasPawn = false;
-                newCell.pawnType = "";
+                newCell.addEventListener("click", handleTileClick);
+                newCell.pawn = undefined;
             }
 
             cells.push(newCell);
@@ -44,15 +75,14 @@ function initBoard() {
 
 //places a pawn element inside of a checkers cell.
 function placePawn(cell, color) {
-    if (color !== "white" && color !== "red") throw Error("placePawn: No Color!");
+    if (color !== "white" && color !== "red") throw Error("placePawn: Bad Color!");
     if (!cell) throw Error("placePawn: Bad cell!");
+
     const newPawn = document.createElement("img");
     newPawn.setAttribute("src", `assets/${color}-piece.png`);
     newPawn.setAttribute("class", `pawn ${color}-pawn`);
+    cell.pawn = new Pawn(newPawn, color, cell);
     cell.appendChild(newPawn);
-    cell.pawnElem = newPawn;
-    cell.hasPawn = true;
-    cell.pawnType = `${color}`;
 }
 
 function initPawns() {
@@ -77,24 +107,73 @@ function initPawns() {
     }
 }
 
-function scanForMoves() {
-    if (!this.hasPawn) return;
-    const position = cells.indexOf(this);
-    if (this.pawnType === "white") {
-        if (position % 8 > 0 && !cells[position - 9].hasPawn) {
-            //higlight and listen for clicks
+function clearActivateCells() {
+    while (activeCells.length > 0) {
+        activeCells[activeCells.length - 1].classList.remove("highlighted");
+        activeCells.pop();
+    }
+}
+
+function activateCell(cell) {
+    cell.classList.add("highlighted");
+    activeCells.push(cell);
+    console.log(cell);
+}
+
+function calculateMove(position, n, clickedCell) {
+    /* 
+        It and highlights white squares on H and A columns. It doesnt affect gameplay though. a pawn can't go on that square because those squares do not listen for clicks. The problem exists, because "if (position % 8 > 0)" checks if single-jumps are possible. there is however nothing that checks for double-jumps. new function can be added - calculateDoubleMove(), but that would be very inconvinient.
+
+            *I could check if n variable equals to something(ie 7) and move on from there.
+     */
+    if (!cells[position + n].pawn) {
+        activateCell(cells[position + n]);
+    } else if (
+        cells[position + n * 2] &&
+        !cells[position + n * 2].pawn &&
+        cells[position + n].pawn.color != clickedCell.pawn.color
+    ) {
+        activateCell(cells[position + n * 2]);
+    }
+}
+
+function highlightLegalMoves(clickedCell) {
+    const position = cells.indexOf(clickedCell);
+    if (clickedCell.pawn.color === "white") {
+        if (position % 8 > 0) {
+            //Check top left
+            calculateMove(position, -9, clickedCell);
         }
-        if (position % 8 < 7 && !cells[position - 7].hasPawn) {
-            //higlight and listen for clicks
+        if (position % 8 < 7) {
+            //Check top right
+            calculateMove(position, -7, clickedCell);
         }
-    } else {
-        if (position % 8 > 0 && !cells[position + 7].hasPawn) {
-            //higlight and listen for clicks
+    } else { //if color === red
+        if (position % 8 < 7) {
+            //Check bottom right
+            calculateMove(position, +9, clickedCell);
         }
-        if (position % 8 < 7 && !cells[position + 9].hasPawn) {
-            //higlight and listen for clicks
+        if (position % 8 > 0) {
+            //Check bottom Left
+            calculateMove(position, +7, clickedCell);
         }
     }
+}
+
+function handleTileClick() {
+    if (selectedPawn && activeCells.includes(this)) {
+        selectedPawn.moveFromTo(selectedPawn.currentCell, this);
+        clearActivateCells();
+        selectedPawn = undefined;
+    }
+    if (!this.pawn) {
+        selectedPawn = undefined;
+        clearActivateCells();
+        return;
+    }
+    if (activeCells.length > 0) clearActivateCells();
+    selectedPawn = this.pawn;
+    highlightLegalMoves(this);
 }
 
 function startGame() {
@@ -102,7 +181,6 @@ function startGame() {
     startMsg.classList.add("hidden");
     gameRunning = true;
 }
-
 
 // Main
 
